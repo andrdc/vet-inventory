21ce8421b6edab36562a7b5c9a888b11e1598daa
Andres Ruiz <andrsruiz@gmail.com>
2020-07-29 17:19:50 -0500

    Add Lash instances

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,10 +1,18 @@
 <template>
 	<div class="home">
+		<h2 class="is-size-3">Food</h2>
 		<div class="instance" v-for="(foodinstance, index) in foodinstances" :key="index">
 			{{ foodinstance.food.type }}
 			{{ foodinstance.food.brand.name }}
 			{{ foodinstance.receive_date }}
 			{{ foodinstance.expiration_date }}
+		</div>
+		<h2 class="is-size-3">Leash</h2>
+		<div class="instance" v-for="(leashinstance, index) in leashinstances" :key="index">
+			{{ leashinstance.leash.name }}
+			{{ leashinstance.leash.material }}
+			{{ leashinstance.leash.brand.name }}
+			{{ leashinstance.receive_date }}
 		</div>
 	</div>
 </template>
@@ -16,11 +24,12 @@ export default {
 	name: "home",
 	data(){
 		return{
-			foodinstances: null
+			foodinstances: null,
+			leashinstances: null
 		}
 	},
 	methods: {
-		getInstances(){
+		getFoodInstances(){
 			axios.get(process.env.VUE_APP_FOOD_INSTANCES)
 				 .then((response) => {
 					console.log(response);
@@ -38,10 +47,30 @@ export default {
 					}
 					console.log(error.config);
 				});
+		},
+		getLeashInstances(){
+			axios.get(process.env.VUE_APP_LEASH_INSTANCES)
+				 .then((response) => {
+					console.log(response);
+					this.leashinstances = response.data;
+				})
+				 .catch((error) => {
+					if(error.response){
+						console.log(error.response.data);
+						console.log(error.response.status);
+						console.log(error.response.headers);
+					}else if(error.request){
+						console.log(error.request);
+					}else{
+						console.log('Error', error.message);
+					}
+					console.log(error.config);
+				});
 		}
 	},
 	mounted(){
-		this.getInstances();
+		this.getFoodInstances();
+		this.getLeashInstances();
 	}
 }
 </script>
