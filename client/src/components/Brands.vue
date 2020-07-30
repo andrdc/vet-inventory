347a6e2a40d6841cec22290973084b5fb89fb019
Andres Ruiz <andrsruiz@gmail.com>
2020-07-30 12:02:15 -0500

    Create Brands Component

---
+++ client/src/components/Brands.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="brands">
+		<h2 class="is-size-3">Brands</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'brands',
+	data(){
+		return{
+		}
+	},
+	methods: {
+	}
+}
+</script>
+
+<style scoped>
+.brands {
+	margin: 5px;
+	padding: 5px;
+}
+</style>
