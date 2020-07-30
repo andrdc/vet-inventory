78dbc35505fb9184ef3e04d4dad082d7f31e4369
Andres Ruiz <andrsruiz@gmail.com>
2020-07-30 13:02:44 -0500

    Create Leashes Component

---
+++ client/src/components/Leashes.vue
@@ -0,0 +1,76 @@
+<template>
+	<div class="leashes">
+		<h2 class="is-size-3">Leashes</h2>
+		<div class="leashes-container">
+			<div class="leash"
+				 @click="goToFood()"
+				 v-for="(leash, index) in leashes"
+				 :key="index">
+				{{ leash.name }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+	name: 'leashs',
+	data(){
+		return{
+			leashes: null
+		}
+	},
+	methods: {
+		goToLeash(){
+			console.log('You clicked a leash');
+		},
+		/* Get all the leashs from the API
+		/* @param none : none
+		/* @return none : none */
+		getLeashes(){
+			axios.get(process.env.VUE_APP_FOODS).then((response) => {
+				console.log(response);
+				this.leashes = response.data;
+			}).catch((error) => {
+				if(error.response){
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				}else if(error.request){
+					console.log(error.request);
+				}else{
+					console.log('Error', error.message);
+				}
+				console.log(error.config);
+			});
+		}
+	},
+	mounted(){
+		this.getLeashes();
+	}
+}
+</script>
+
+<style scoped>
+.leashes {
+	margin: 5px;
+	padding: 5px;
+}
+
+.leashes-container {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: flex-start;
+	align-content: flex-start;
+}
+
+.leash {
+	border: 1px solid black;
+	border-radius: 5px;
+	margin: 10px;
+	padding: 10px;
+	cursor: pointer;
+}
+</style>
