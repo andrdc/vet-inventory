9b677b6f0a482f12b772d457b1c50ad963985ee4
Andres Ruiz <andrsruiz@gmail.com>
2020-07-31 12:53:32 -0500

    Create POST method to create Food

--- server/controllers/food.js
+++ server/controllers/food.js
@@ -13,3 +13,23 @@ exports.foods = (req, res, next) => {
 			res.json(foods);
 		});
 };
+
+/* Create new Food */
+exports.create_food = (req, res, next) => {
+	let food = new Food({
+		type: req.body.type,
+		weigth: req.body.weigth,
+		price_vet: req.body.price_vet,
+		price_public: req.body.price_public,
+		brand: req.body.brand,
+		name: req.body.name
+	});
+
+	food.save((err) => {
+		if(err)
+			return next(err);
+
+		/* Success */
+		res.status(201).json(food);
+	})
+}
