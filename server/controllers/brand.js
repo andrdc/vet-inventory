2b5313ce34995e61e4b2ab6971b4592d21db7e61
Andres Ruiz <andrsruiz@gmail.com>
2020-07-31 09:52:07 -0500

    Create post method to create Brand

--- server/controllers/brand.js
+++ server/controllers/brand.js
@@ -12,3 +12,18 @@ exports.brands = (req, res, next) => {
 		res.json(brands);
 	});
 };
+
+/* Create new Brand */
+exports.create_brand = (req, res, next) => {
+	let brand = new Brand({
+		name: req.body.name,
+	});
+
+	brand.save((err) => {
+		if(err)
+			return next(err);
+
+		/* Success */
+		res.status(201).json(brand);
+	});
+};
