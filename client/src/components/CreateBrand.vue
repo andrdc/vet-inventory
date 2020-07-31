2b59ea64fb38faac86eea181d84fc68057d1ec15
Andres Ruiz <andrsruiz@gmail.com>
2020-07-31 11:40:50 -0500

    Add media queries to resize form

--- client/src/components/CreateBrand.vue
+++ client/src/components/CreateBrand.vue
@@ -1,12 +1,18 @@
 <template>
 	<div class="new-brand">
-		<form>
-			<section>
-				<b-field label="Name">
-					<b-input v-model="name"></b-input>
-				</b-field>
-			</section>
-		</form>
+		<h2 class="is-size-3 has-text-primary">Create Brand</h2>
+		<div class="form-container">
+			<form class="form">
+				<section>
+					<b-field label="Name">
+						<b-input v-model="name"></b-input>
+					</b-field>
+				</section>
+				<section>
+					<b-input type="submit"></b-input>
+				</section>
+			</form>
+		</div>
 	</div>
 </template>
 
@@ -28,5 +34,36 @@ export default {
 .new-brand {
 	margin: 5px;
 	padding: 5px;
+}
+
+.form-container {
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: center;
+	align-items: center;
+}
+
+@media all and (max-width: 2000px) {
+	.form {
+		width: 30%;
+	}
+}
+
+@media all and (max-width: 1000px) {
+	.form {
+		width: 50%;
+	}
+}
+
+@media all and (max-width: 800px) {
+	.form {
+		width: 70%;
+	}
+}
+
+@media all and (max-width: 600px) {
+	.form {
+		width: 100%;
+	}
 }
 </style>
