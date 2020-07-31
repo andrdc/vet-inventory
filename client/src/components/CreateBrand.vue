146ef410a79ef0a3d2fd556ec2b251015ecd1017
Andres Ruiz <andrsruiz@gmail.com>
2020-07-31 09:53:03 -0500

    Create CreateBrand component

---
+++ client/src/components/CreateBrand.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="new-brand">
+		<form>
+			<section>
+				<b-field label="Name">
+					<b-input v-model="name"></b-input>
+				</b-field>
+			</section>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'create-brand',
+	data(){
+		return{
+			name: ''
+		}
+	},
+	methods: {
+		createBrand(){ console.log('Create new Brand'); }
+	}
+}
+</script>
+
+<style scoped>
+.new-brand {
+	margin: 5px;
+	padding: 5px;
+}
+</style>
