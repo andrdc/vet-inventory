0c413b7fbb08eb1ddafbda91b1a7735ec214c9f8
Andres Ruiz <andrsruiz@gmail.com>
2020-07-31 12:07:27 -0500

    Add watch property to enabled submit button

--- client/src/components/CreateBrand.vue
+++ client/src/components/CreateBrand.vue
@@ -4,13 +4,29 @@
 		<div class="form-container">
 			<form class="form">
 				<section>
-					<b-field label="Name">
-						<b-input v-model="name"></b-input>
+					<b-field>
+						<template slot="label">
+							Name
+							<b-tooltip type="is-light" label="Name of the brand">
+								<b-icon size="is-small"
+										pack="fas"
+										icon="question-circle">
+								</b-icon>
+							</b-tooltip>
+						</template>
+						<b-input type="text"
+								 maxlength=20
+								 v-model="name"></b-input>
 					</b-field>
 				</section>
-				<section>
-					<b-input type="submit"></b-input>
-				</section>
+				<br>
+				<span class="submit">
+					<b-button type="is-success"
+							  :disabled=submitDisabled
+							  @click="createBrand()">
+						Send
+					</b-button>
+				</span>
 			</form>
 		</div>
 	</div>
@@ -21,11 +37,23 @@ export default {
 	name: 'create-brand',
 	data(){
 		return{
-			name: ''
+			name: '',
+			submitDisabled: true
 		}
 	},
 	methods: {
-		createBrand(){ console.log('Create new Brand'); }
+		createBrand(){
+			console.log('Create new Brand: ' + this.name );
+			this.name = '';
+		}
+	},
+	watch: {
+		name: function(){
+			if(this.name.length > 4)
+				this.submitDisabled = false;
+			else
+				this.submitDisabled = true;
+		}
 	}
 }
 </script>
@@ -65,5 +93,11 @@ export default {
 	.form {
 		width: 100%;
 	}
+}
+
+.submit {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
 }
 </style>
