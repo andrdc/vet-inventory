25909c86628b15a517f3247a91074865835ddbf0
Andres Ruiz <andrsruiz@gmail.com>
2020-08-03 16:39:10 -0500

    Add create leash instance POST method

--- server/controllers/leashinstance.js
+++ server/controllers/leashinstance.js
@@ -13,3 +13,19 @@ exports.leashinstances = (req, res, next) => {
 					res.json(leashinstances);
 				});
 };
+
+/* Create Leash Instance */
+exports.create_leash_instance = (req, res, next) => {
+	let leashinstance = new LeashInstance({
+		leash: req.body.leash,
+		receive_date: req.body.receive_date
+	});
+
+	leashinstance.save((err) => {
+		if(err)
+			return next(err);
+
+		/* Success */
+		res.status(201).json(leashinstance);
+	});
+};
