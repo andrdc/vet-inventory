c5aa3f50cca100262970c9c97a2e5dc4d3297d1a
Andres Ruiz <andrsruiz@gmail.com>
2020-08-04 16:12:11 -0500

    Add router-link to go to specific food element

--- client/src/components/Foods.vue
+++ client/src/components/Foods.vue
@@ -12,12 +12,15 @@
 		</div>
 		<div class="foods-container">
 			<div class="food"
-				 @click="goToFood()"
 				 v-for="(food, index) in foods"
 				 :key="index">
-				{{ food.name }}, {{ food.weigth }} KG.
-				<br>
-				({{ food.type }}), {{ food.brand.name }}.
+				<b-button tag="router-link"
+						  :to="{ path: 'food/update', query: { id: food._id } }"
+						  type="is-light">
+					{{ food.name }}, {{ food.weigth }} KG.
+					<br>
+					({{ food.type }}), {{ food.brand.name }}.
+				</b-button>
 			</div>
 		</div>
 		<div class="error" v-if="isFoodError">{{ foodError }}</div>
@@ -25,43 +28,40 @@
 </template>
 
 <script>
- import axios from 'axios';
+import axios from 'axios';
 
- export default {
-	 name: 'foods',
-	 data(){
-		 return{
-			 foods: null,
-			 isFoodError: false,
-			 foodError: 'Error : '
-		 }
-	 },
-	 methods: {
-		 goToFood(){
-			 console.log('You clicked a food');
-		 },
-		 /* Get all the foods from the API
-			/* @param none : none
-			/* @return none : none */
-		 getFoods(){
-			 axios.get(process.env.VUE_APP_FOODS).then((response) => {
-				 this.foods = response.data;
-			 }).catch((error) => {
-				 this.isFoodError = true;
-				 if(error.response){
-					 this.foodError += (error.response.data + error.response.status + error.response.headers);
-				 }else if(error.request){
-					 this.foodError += error.request;
-				 }else{
-					 this.foodError += error.message;
-				 }
-			 });
-		 }
-	 },
-	 mounted(){
-		 this.getFoods();
-	 }
- }
+export default {
+	name: 'foods',
+	data(){
+		return{
+			foods: null,
+			isFoodError: false,
+			foodError: 'Error : '
+		}
+	},
+	methods: {
+		/* Get all the foods from the API
+		/* @param none : none
+		/* @return none : none */
+		getFoods(){
+			axios.get(process.env.VUE_APP_FOODS).then((response) => {
+				this.foods = response.data;
+			}).catch((error) => {
+				this.isFoodError = true;
+				if(error.response){
+					this.foodError += (error.response.data + error.response.status + error.response.headers);
+				}else if(error.request){
+					this.foodError += error.request;
+				}else{
+					this.foodError += error.message;
+				}
+			});
+		}
+	},
+	mounted(){
+		this.getFoods();
+	}
+}
 </script>
 
 <style scoped>
