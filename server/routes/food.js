d3686b83611ce6ae343c9ef6e08b7d391c17a59c
Andres Ruiz <andrsruiz@gmail.com>
2020-08-04 15:57:18 -0500

    Set Route for Find Food API's method

--- server/routes/food.js
+++ server/routes/food.js
@@ -8,5 +8,6 @@ var foodController = require(path.join(__dirname, '../controllers/food'));
 /* Food Routes */
 router.get('/', foodController.foods);
 router.post('/', foodController.create_food);
+router.get('/find/:id', foodController.find_food);
 
 module.exports = router;
