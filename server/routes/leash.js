80a264e667cddbed8f33f8a49129ce4b9d29b1e5
Andres Ruiz <andrsruiz@gmail.com>
2020-08-06 12:52:38 -0500

    Set Route for POST delete method, Leash

--- server/routes/leash.js
+++ server/routes/leash.js
@@ -10,5 +10,6 @@ router.get('/', leashController.leashes);
 router.post('/', leashController.create_leash);
 router.get('/find/:id', leashController.find_leash);
 router.post('/update/:id', leashController.update_leash);
+router.post('/delete/:id', leashController.delete_leash);
 
 module.exports = router;
